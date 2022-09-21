22b156ac97c748b606d116b0e55c323628fd065c
harshvyas23 <harshvyasm23@gmail.com>
2022-09-21 12:45:15 +0530

    project_Initiated

---
+++ Day1/my_first_code.py
@@ -0,0 +1,5 @@
+env=input("Enter the cloud platform:")
+if env == "aws" or env == "AWS":
+    print("you are in AWS Enviroment")
+else:
+    print("you are not in AWS enviroment")
---
+++ Day2/list_ex.py
@@ -0,0 +1,5 @@
+from re import T
+
+
+list_of_names = list()
+print(type(list_of_names))
